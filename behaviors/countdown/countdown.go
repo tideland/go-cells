189e69e49f93cc40971ad49f75ec026b6a44f15c
Frank Mueller <frank.mueller@themue.dev>
2021-08-27 16:20:16 +0200

    Move countdown behavior

---
+++ behaviors/countdown/countdown.go
@@ -0,0 +1,70 @@
+// Tideland Go Cells - Behaviors - Countdown
+//
+// Copyright (C) 2010-2021 Frank Mueller / Tideland / Oldenburg / Germany
+//
+// All rights reserved. Use of this source code is governed
+// by the new BSD license.
+
+package countdown // import "tideland.dev/go/cells/behaviors/countdown"
+
+//--------------------
+// IMPORTS
+//--------------------
+
+import (
+	"tideland.dev/go/cells/mesh"
+)
+
+//--------------------
+// HELPER
+//--------------------
+
+// ZeroFunc is called when the countdown reaches zero. The collected
+// events are passed, the returned event will be emitted.
+type ZeroFunc func(r mesh.EventSinkReader) (*mesh.Event, error)
+
+//--------------------
+// BEHAVIOR
+//--------------------
+
+// Behavior collects a number of events. When this number is reached
+// a zero function with access to these events will be called. The event
+// returned by this function will be emitted and the counter reset.
+type Behavior struct {
+	t      int
+	zeroer ZeroFunc
+}
+
+var _ mesh.Behavior = &Behavior{}
+
+// New creates a countdown behavior based on the passed
+// t value and zeroer function.
+func New(t int, zeroer ZeroFunc) *Behavior {
+	return &Behavior{
+		t:      t,
+		zeroer: zeroer,
+	}
+}
+
+// Go implements the mesh.Behavior interface.
+func (b *Behavior) Go(cell mesh.Cell, in mesh.Receptor, out mesh.Emitter) error {
+	sink := mesh.NewEventSink(b.t)
+	for {
+		select {
+		case <-cell.Context().Done():
+			return nil
+		case evt := <-in.Pull():
+			l := sink.Push(evt)
+			if l == b.t {
+				outEvt, err := b.zeroer(sink)
+				if err != nil {
+					return err
+				}
+				out.EmitEvent(outEvt)
+				sink = mesh.NewEventSink(b.t)
+			}
+		}
+	}
+}
+
+// EOF
--- behaviors/countdown/countdown_test.go
+++ behaviors/countdown/countdown_test.go
@@ -1,11 +1,11 @@
-// Tideland Go Cells - Behaviors - Unit Tests
+// Tideland Go Cells - Behaviors - Countdown - Unit Tests
 //
 // Copyright (C) 2010-2021 Frank Mueller / Tideland / Oldenburg / Germany
 //
 // All rights reserved. Use of this source code is governed
 // by the new BSD license.
 
-package behaviors_test // import "tideland.dev/go/cells/behaviors"
+package countdown_test // import "tideland.dev/go/cells/behaviors/countdown"
 
 //--------------------
 // IMPORTS
@@ -18,7 +18,7 @@ import (
 	"tideland.dev/go/audit/asserts"
 	"tideland.dev/go/audit/generators"
 
-	"tideland.dev/go/cells/behaviors"
+	"tideland.dev/go/cells/behaviors/countdown"
 	"tideland.dev/go/cells/mesh"
 )
 
@@ -26,8 +26,8 @@ import (
 // TESTS
 //--------------------
 
-// TestCountdownBehavior tests the countdown of events.
-func TestCountdownBehavior(t *testing.T) {
+// TestSuccess verifies the countdown of events.
+func TestSuccess(t *testing.T) {
 	assert := asserts.NewTesting(t, asserts.FailStop)
 	generator := generators.New(generators.FixedRand())
 	topics := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i"}
@@ -38,16 +38,16 @@ func TestCountdownBehavior(t *testing.T) {
 		}
 		return nil, nil
 	}
-	behavior := behaviors.NewCountdownBehavior(count, zeroer)
+	behavior := countdown.New(count, zeroer)
 	// Test evaluation.
-	eval := func(tbe *mesh.TestbedEvaluator, evt *mesh.Event) error {
+	test := func(tbe *mesh.TestbedEvaluator, evt *mesh.Event) error {
 		if evt.Topic() == "length-ok" {
 			tbe.SetSuccess()
 		}
 		return nil
 	}
 	// Run test.
-	tb := mesh.NewTestbed(behavior, eval)
+	tb := mesh.NewTestbed(behavior, test)
 	err := tb.Go(func(out mesh.Emitter) {
 		for i := 0; i < count; i++ {
 			topic := generator.OneStringOf(topics...)
