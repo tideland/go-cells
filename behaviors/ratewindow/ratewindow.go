bf1374ef6e5ad4626785eac64235601da379b31b
themue <frank.mueller@themue.dev>
2022-12-28 21:58:51 +0000

    Start with RateWindowBehavior
    Signed-off-by: themue <frank.mueller@themue.dev>

---
+++ behaviors/ratewindow/ratewindow.go
@@ -0,0 +1,117 @@
+// Tideland Go Cells - Behaviors - Rate Window Evaluator
+//
+// Copyright (C) 2010-2022 Frank Mueller / Tideland / Oldenburg / Germany
+//
+// All rights reserved. Use of this source code is governed
+// by the new BSD license.
+
+package rateevaluator // import "tideland.dev/go/cells/behaviors/ratewindow"
+
+//--------------------
+// IMPORTS
+//--------------------
+
+import (
+	"time"
+
+	"tideland.dev/go/cells/mesh"
+)
+
+//--------------------
+// CONSTANTS
+//--------------------
+
+const (
+	// TopicRateWindow signals a detected event rate window.
+	TopicRateWindow = "rate-window"
+)
+
+//--------------------
+// RATE WINDOW BEHAVIOR
+//--------------------
+
+// RateWindowCriterion is used by the rate window behavior and has to return
+// true, if the passed event matches a criterion for rate window measuring.
+type RateWindowCriterion func(evt *mesh.Event) (bool, error)
+
+// Behavior implements the rate window behavior.
+type Behavior struct {
+	matches  RateWindowCriterion
+	count    int
+	duration time.Duration
+
+	cell    cells.Cell
+	sink    *mesh.EventSink
+	process cells.EventSinkProcessor
+}
+
+// NewRateWindowBehavior creates an event rate window behavior. It checks
+// if an event matches the passed criterion. If count events match during
+// duration the process function is called. Its returned payload is
+// emitted as new event with topic "rate-window". A received "reset" as
+// topic resets the collected matches.
+func NewRateWindowBehavior(
+	matches RateWindowCriterion,
+	count int,
+	duration time.Duration,
+	process cells.EventSinkProcessor) cells.Behavior {
+	return &rateWindowBehavior{
+		sink:     cells.NewEventSink(count),
+		matches:  matches,
+		count:    count,
+		duration: duration,
+		process:  process,
+	}
+}
+
+// Init implements the cells.Behavior interface.
+func (b *rateWindowBehavior) Init(c cells.Cell) error {
+	b.cell = c
+	return nil
+}
+
+// Terminate implements the cells.Behavior interface.
+func (b *rateWindowBehavior) Terminate() error {
+	return nil
+}
+
+// ProcessEvent implements the cells.Behavior interface.
+func (b *rateWindowBehavior) ProcessEvent(event cells.Event) error {
+	switch event.Topic() {
+	case cells.TopicReset:
+		b.sink.Clear()
+	default:
+		ok, err := b.matches(event)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return nil
+		}
+		b.sink.Push(event)
+		if b.sink.Len() == b.count {
+			// Got enough matches, check duration.
+			first, _ := b.sink.PeekFirst()
+			last, _ := b.sink.PeekLast()
+			difference := last.Timestamp().Sub(first.Timestamp())
+			if difference <= b.duration {
+				// We've got a burst!
+				payload, err := b.process(b.sink)
+				if err != nil {
+					return err
+				}
+				b.cell.EmitNew(TopicRateWindow, payload)
+			}
+			b.sink.PullFirst()
+		}
+	}
+	return nil
+}
+
+// Recover implements the cells.Behavior interface.
+func (b *rateWindowBehavior) Recover(err interface{}) error {
+	b.sink = cells.NewEventSink(b.count)
+	return nil
+}
+
+// EOF
